b560ee393e80a18a56f6ea3bb6d04283b081a8be
zannet <72852996+zannetdev@users.noreply.github.com>
2022-07-19 18:05:29 -0500

    Update const.php

--- server/config/const.php
+++ server/config/const.php
@@ -4,4 +4,4 @@ define('MB', 1048576);
 define('GB', 1073741824);
 define('TB', 1099511627776);
 define('URL', 'http://localhost/carpeta/');
-define('LIBS', 'server/libs/');
+define('LIBS', 'server/lib/');
